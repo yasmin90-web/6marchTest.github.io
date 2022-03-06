a6c9d06d23409aade37b1587f56f4ccfff99aae9
yasmin90-web <96856534+yasmin90-web@users.noreply.github.com>
2022-03-06 18:48:41 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+
+function myFunctionsecond() {
+    var popup2 = document.getElementById("myPopup2");
+    popup2.classList.toggle("show");
+}
+function myFunctionthird() {
+    var popup3 = document.getElementById("myPopup3");
+    popup3.classList.toggle("show");
+}
+function myFunctionthird() {
+    var popup3 = document.getElementById("myPopup3");
+    popup3.classList.toggle("show");
+}
+function myFunctionfourth() {
+    var popup4 = document.getElementById("myPopup4");
+    popup4.classList.toggle("show");
+}
+function myFunctionfifth() {
+    var popup5 = document.getElementById("myPopup5");
+    popup5.classList.toggle("show");
+}
